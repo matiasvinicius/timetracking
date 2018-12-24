35b080f81e7b03cd822a30b23f606074ae67f92e
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2018-12-24 03:54:11 -0200

    Update chekin.rb
    Alterando validação desnecessária

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -3,6 +3,6 @@ class Checkin < ApplicationRecord
   #validation - server
   validates :typeCheckin, presence: {message: ":write a comment."}
   validates :latitude, :longitude, presence: {message: ": ‎make sure javascript is enabled."}
-  validates :created_at, presence: {message: ":created_at can not be registered"}
+  #created_at is automatic
 
 end
